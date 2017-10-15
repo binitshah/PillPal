3891e36f6e1a135911dbf84de70253b2e2e25ff8
Binit Shah <bshah@gatech.edu>
2017-10-15 09:25:32 -0400

    help im tired

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import jsonify
 from random import randint
 import pymongo
+from datetime import date, datetime, timedelta
 
 app = Flask(__name__)
 uri = 'mongodb://heroku_qg4xxhbq:2jb487u6ota241oqoh7vgpd9tj@ds119210.mlab.com:19210/heroku_qg4xxhbq'
@@ -10,14 +11,8 @@ db = client.get_default_database()
 
 @app.route('/')
 def index():
-    devices = db['devices']
-    devices.insert_many(SEED_DATA)
-    cursor = devices.find({'id': "31562"})
-    message = ""
-    for device in cursor:
-        numProfiles = len(device['profiles'])
-        message = f"hey, you have got {numProfiles} profiles."
-    return message
+    convert_frequency_and_time_to_date(datetime(2017, 10, 3), 4, "8:00")
+    return "Hi! Welcome to PillPal!"
 
 @app.route('/get_device_profiles/<device_id>')
 def get_device_profiles(device_id):
@@ -25,8 +20,8 @@ def get_device_profiles(device_id):
     devices_cursor = devices_db.find({'device_id': device_id})
     profile_ids = []
     for device in devices_cursor:
-        for profile in device['profile_ids']:
-            profile_ids.append(profile)
+        for profile_id in device['profile_ids']:
+            profile_ids.append(profile_id)
     if (len(profile_ids) > 0):
         profiles = []
         for profile_id in profile_ids:
@@ -39,37 +34,84 @@ def get_device_profiles(device_id):
             profile['profile_id'] = profile_id
             profile['name'] = name
             profiles.append(profile)
-        return jsonify(hasProfiles=True,
-                        profiles=profiles)
+        return jsonify(profiles=profiles)
     else:
-        return jsonify(hasProfiles=False,
-                        profiles=[])
+        return jsonify(profiles=[])
 
 @app.route('/create_device_profile/<device_id>/<name>/<color>')
 def create_device_profile(device_id, name, color):
     profile_id = name.split()[0] + str(random_with_N_digits(5))
     profiles_db = db["profiles"]
-    profiles_db.insert_one({'profile_id': profile_id, 'name': name, 'color': color})
+    profiles_db.insert_one({'profile_id': profile_id, 'name': name, 'color': color, 'prescription_ids': []})
     # determine whether the device already exists
     devices_db = db['devices']
     device_cursor = devices_db.find({'device_id': device_id})
-    if(device_cursor.count() > 0):
+    if (device_cursor.count() > 0):
         # device exists, let's recreate profile list and append to it new user
-        # device = device_cursor[0]
-        # profile_ids = device['profile_ids']
-        # profile_ids.append(profile_id)
         devices_db.update({'device_id': device_id}, {'$push': {'profile_ids': profile_id}})
-        return profile_id
+        return jsonify(profile_id=profile_id)
     else:
         # device doesn't exist, let's start new profile list and save the new device
         profile_ids = [profile_id]
         devices_db.insert_one({'device_id': device_id, 'profile_ids': profile_ids})
-        return profile_id
+        return jsonify(profile_id=profile_id)
 
 def random_with_N_digits(n):
     range_start = 10**(n-1)
     range_end = (10**n)-1
     return randint(range_start, range_end)
+
+@app.route('/get_next_prescription/<device_id>')
+def get_next_prescription(device_id):
+    devices_db = db['devices']
+    devices_cursor = devices_db.find({'device_id': device_id})
+    profile_ids = []
+    for device in devices_cursor:
+        for profile_id in device['profile_ids']:
+            profile_ids.append(profile_id)
+        if (len(profile_ids) > 0):
+            for profile_id in profile_ids:
+                profiles_db = db["profiles"]
+                profile_cursor = profiles_db.find({'profile_id': profile_id})
+                if (profile_cursor.count() > 0):
+                    # profile exists, which is normal. Let's get the prescriptions
+                    profile = profile_cursor[0]
+                    prescription_ids = profile['prescription_ids']
+                    if (len(prescription_ids) > 0):
+                        # figure out next prescription and send it back
+                        prescriptions = []
+                        prescriptions_db = db['prescriptions']
+                        for prescription_id in prescription_ids:
+                            prescription_cursor = prescriptions_db.find({'prescription_id': prescription_id})
+                            if (prescription_cursor.count() > 0):
+                                prescriptions.append(prescription_cursor[0])
+                        
+                        prescridoses_dates = []
+                        # given each prescription, we'll need to get doses
+                        for prescription in prescriptions:
+                            prescridoses_dates.append(convert_frequency_and_time_to_date(prescription['start_date'], prescription['frequency'], prescription['time']))
+
+                        # sort
+                        prescridoses_dates.sort()
+                        return jsonify(error=False, next_prescription=prescridoses_dates[0])
+
+                    else:
+                        # no prescriptions to deal with
+                        return jsonify(error=False, next_prescription="")
+                else:
+                    # well shit, so the profile doesn't exist
+                    return jsonify(error=True, next_prescription="")
+
+def convert_frequency_and_time_to_date(start_date, frequency, time):
+    time_bits = time.split(":")
+    hour = int(time_bits[0]) - 1
+    minute = int(time_bits[1]) - 1
+    if (minute < 0):
+        minute = 0
+    start_date = datetime(start_date.year, start_date.month, start_date.day, hour, minute)
+    while (start_date < datetime.now()):
+        start_date = start_date + timedelta(days=frequency)
+    return start_date
 
 if __name__ == "__main__":
     app.run()
