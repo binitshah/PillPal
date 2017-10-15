25f08f95dc3af755aa18139b6ad0b379aa3a9641
Max Rudolph <mrudolph8@gatech.edu>
2017-10-15 06:07:32 -0400

    Now with updated things

--- wifiMain.cpp
+++ wifiMain.cpp
@@ -9,57 +9,31 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
-#include <WiFi.h>
+#include <ESP8266WiFi.h>
+
 
 
 String receiveCommandString2();
 
-char ssid[] = "Max iPhone"; //  your network SSID (name) 
-char pass[] = "23smjt5j43rts";    // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;            // your network key Index number (needed only for WEP)
 
-int status = WL_IDLE_STATUS;
-// if you don't want to use DNS (and reduce your sketch size)
-// use the numeric IP instead of the name for the server:
-//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-char server[] = "www.google.com";    // name address for Google (using DNS)
-
-// Initialize the Ethernet client library
-// with the IP address and port of the server 
-// that you want to connect to (port 80 is default for HTTP):
 WiFiClient client;
 
 void setup() {
-  //Initialize serial and wait for port to open:
-  Serial.begin(9600); 
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
-  
+  Serial.begin(9600);
+  Serial.println();
 
-  // attempt to connect to Wifi network:
-  while (status != WL_CONNECTED) { 
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
-    status = WiFi.begin(ssid, pass);
-  
-    // wait 10 seconds for connection:
-    delay(10000);
-  } 
-  Serial.println("Connected to wifi");
-  printWifiStatus();
-  
-  Serial.println("\nStarting connection to server...");
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 80)) {
-    Serial.println("connected to server");
-    // Make a HTTP request:
-    client.println("GET /search?q=arduino HTTP/1.1");
-    client.println("Host: www.google.com");
-    client.println("Connection: close");
-    client.println();
+  WiFi.begin("Max iPhone", "23smjt5j43rts");
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
   }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
